060432ff2173a0197c47251b546ac204273ef58d
Rickey Lee <me@rickey.co.nz>
2025-10-19 10:22:10 +1300

    ios fix

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 export default function Section({ id, className = '', children }) {
   return (
-    <section id={id} className={['snap-start min-h-screen', className].join(' ')}>
+    <section id={id} className={['snap-start min-h-dvh', className].join(' ')}>
       {children}
     </section>
   )
 }
+
