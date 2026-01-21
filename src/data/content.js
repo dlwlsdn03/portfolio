7277d03ead9cf2a23acb48c8ca9829043bd74943
Rickey Lee <me@rickey.co.nz>
2026-01-21 19:04:01 +1300

    Update content.js

--- src/data/content.js
+++ src/data/content.js
@@ -96,13 +96,13 @@ export const education = [
 export const publications = [
   {
     
-    title: 'Lee, R. & Case, J. (2026). Volatility Forecasting using Statistically Disciplined Model Memory. Working Paper.',
+    title: 'Lee, R. & Case, J. (2026). Statistical model memory selection in volatility forecasting. Working Paper.',
     venue: '',
     year: '',
     link: '',
   },
   {
-    title: ' Lee, R. (2026). Adaptive Autoregressive Model Selection in Financial Time Series. Working Paper.',
+    title: ' Lee, R. (2026). Time-varying model selection using information criterion. Working Paper.',
     venue: '',
     year: '',
     link: '',
