f85900574d8e90696c1fa084ed638d7b145fec03
Rickey Lee <me@rickey.co.nz>
2026-03-29 23:18:40 +1300

    Update content.js

--- src/data/content.js
+++ src/data/content.js
@@ -69,7 +69,7 @@ export const education = [
     thesisLabel: 'Dissertation:',
     thesisTitle: '"Volatility Forecasting Using Effective Degrees of Freedom for Adaptive Complexity Control"',
     thesisLink: '/Honours Dissertation.pdf',
-    details: 'Supervised by Dr. Justin Case\n\n Awards:\n Beta Gamma Sigma Lifetime Member (Top 10%)\n First in Course Award in Research Methods in Finance (FIN701)\n First in Course Award in Financial Machine Learning (FIN710)\n\n Competitions:\n 2nd in Montarne Investment Banking Competition\n 3rd in AcademyGlobal Business Simulation Competition\n\n Leadership and Service:\n Graduate Cohort Representative in the Department of Economics\n Class Representative in Econometric Methods\n Class Representative in Financial Machine Learning\n\nProfessional Development:\nMcKinsey Forward Program\nStartmate Student Fellowship',
+    details: 'Supervised by Dr. Justin Case\n\n Awards:\n Beta Gamma Sigma Lifetime Member (Top 10%)\n First in Course Award in Research Methods in Finance (FIN701)\n First in Course Award in Financial Machine Learning (FIN710)\n\n Competitions:\n 2nd in Montarne Investment Banking Competition\n 3rd in AcademyGlobal Business Simulation Competition\n\n Leadership and Service:\n Graduate Cohort Representative in the Department of Economics\n Class Representative in Econometric Methods (ECON720)\n Class Representative in Financial Machine Learning (FIN710)\n\nProfessional Development:\nMcKinsey Forward Program\nStartmate Student Fellowship',
     logo: 'uoa.jpg',
     logoDark: 'uoa-dark.jpg'
   },
